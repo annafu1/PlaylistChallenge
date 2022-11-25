a1a1882181a6d32976e3ec7937c77f1c520db909
annafu1 <104175541+annafu1@users.noreply.github.com>
2022-11-25 10:26:02 -0500

    completed

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -9,6 +9,30 @@ public class Music {
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+        Integer minPress = Integer.MAX_VALUE;
+        Integer currentIndex = startIndex;
+        for (int i = 0; i < playList.length; i++) {
+            if(playList[(startIndex + i) % playList.length].equals(selection)) {
+                minPress = i;
+                System.out.println("Minimum Press: " + minPress);
+                break;
+            }
+        }
+        for (int i = 1; i < playList.length; i++) {
+            currentIndex--;
+            if (currentIndex < 0) {
+                currentIndex = playList.length - 1;
+            }
+            if (playList[currentIndex].equals(selection)) {
+                //below line allows us to find the smallest max val and i going up/down
+                minPress = Math.min(minPress, i);
+                System.out.println("Minimum Press: " + minPress);
+                break;
+            }
+        }
+        if (minPress.equals(Integer.MAX_VALUE)) {
+            return null;
+        }
+        return minPress;
     }
 }
